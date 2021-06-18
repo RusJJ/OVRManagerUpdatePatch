2fd688666545310e93d2bc048897afd80edf2332
RusJJ <rusjj1337@gmail.com>
2021-06-18 21:31:05 +0300

    Initial release

---
+++ OVRManagerPatch/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ModConstants
+{
+    class ModConstants
+    {
+        public const string modGUID = "net.rusjj.ovrmanager.patch";
+        public const string modName = "OVRManager.Update removal";
+        public const string modVersion = "1.0.0";
+    }
+}
---
+++ OVRManagerPatch/Main.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace OVRManagerPatch
+{
+    [BepInPlugin(ModConstants.ModConstants.modGUID, ModConstants.ModConstants.modName, ModConstants.ModConstants.modVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        public static bool shouldCallFunction = false;
+        void Awake()
+        {
+            HarmonyPatcher.Patch.Apply();
+        }
+        private void OnEnable()
+        {
+            shouldCallFunction = false;
+        }
+        private void OnDisable()
+        {
+            shouldCallFunction = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(OVRManager))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+    internal class OVRManagerUpdate
+    {
+        /* OVRManager::Update flooding... */
+        public static bool Prefix()
+        {
+            return Main.shouldCallFunction;
+        }
+    }
+}
